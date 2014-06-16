1b0cd7eeafa2c89e21582043d58d341b8be6add1
Juan Wajnerman <jwajnerman@manas.com.ar>
2014-06-16 12:40:04 -0300

    Added micoro

---
+++ micoro.rb
@@ -0,0 +1,30 @@
+require "formula"
+
+# Documentation: https://github.com/Homebrew/homebrew/wiki/Formula-Cookbook
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Micoro < Formula
+  homepage "https://github.com/mikewei/micoro"
+  head "https://github.com/mikewei/micoro.git"
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    system "autoreconf -i"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test micoro`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
