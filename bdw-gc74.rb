186f491a459bc3d8db461ab0c051bd152fc5de25
Juan Wajnerman <jwajnerman@manas.com.ar>
2014-06-18 23:41:35 -0300

    Renamed bdw-gc to bdw-gc74 to avoid conflicting with the formula in the main repository

--- bdw-gc74.rb
+++ bdw-gc74.rb
@@ -1,19 +1,12 @@
 require 'formula'
 
-class BdwGc < Formula
+class BdwGc74 < Formula
   homepage 'http://www.hboehm.info/gc/'
   url 'http://www.hboehm.info/gc/gc_source/gc-7.4.0.tar.gz'
   sha1 '82f031a5a6db004df3cf8f1b1e72dd6b313ab032'
 
   depends_on "libatomic_ops"
   depends_on 'pkg-config' => :build
-
-  bottle do
-    cellar :any
-    sha1 "267d3e346e5d8a9fff781ee58a7aa3b33e31b3db" => :mavericks
-    sha1 "a92904c2e10891252bb212eb4a438094a36de8c8" => :mountain_lion
-    sha1 "ecc4b3bded24d5a48255c4c4d5295680d5befcf2" => :lion
-  end
 
   option :universal
 
