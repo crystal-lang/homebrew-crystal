ec623877b7f524e884a3444d07d6b8565a9b380a
Juan Wajnerman <jwajnerman@manas.com.ar>
2014-06-19 00:36:00 -0300

    Allow to install both head and stable versions

--- crystal.rb
+++ crystal.rb
@@ -1,19 +1,44 @@
 require 'formula'
 
 class Crystal < Formula
-  homepage 'http://crystal-lang.org'
-  url 'http://crystal-lang.s3.amazonaws.com/crystal-darwin-0.1.0-p0.tar.gz'
-  sha1 'fbbd7f817daa19a56c5c97128cd9c66c7e99b68c'
+  homepage 'http://crystal-lang.org/'
+
+  stable do
+    url 'http://crystal-lang.s3.amazonaws.com/crystal-darwin-0.1.0-p0.tar.gz'
+    sha1 'fbbd7f817daa19a56c5c97128cd9c66c7e99b68c'
+  end
+
+  head do
+    url 'http://github.com/manastech/crystal.git'
+
+    resource 'latest' do
+      url 'http://crystal-lang.s3.amazonaws.com/crystal-darwin-latest.tar.gz'
+    end
+  end
 
   depends_on "llvm33" => %w(with-clang all-targets)
-  depends_on "manastech/crystal/bdw-gc"
+  depends_on "bdw-gc74"
   depends_on "libtask"
 
   def install
+    if build.head?
+      resource('latest').stage do
+        (prefix/"deps").install "bin/crystal-exe" => "crystal"
+      end
+
+      script_root = %Q(SCRIPT_ROOT="#{prefix}")
+    else
+      script_root = %Q(SCRIPT_ROOT="#{prefix}/bin")
+    end
+
     inreplace('bin/crystal') do |s|
-      s.gsub! /SCRIPT_ROOT=.+/, %Q(SCRIPT_ROOT="#{prefix}/bin")
+      s.gsub! /SCRIPT_ROOT=.+/, script_root
     end
 
     prefix.install Dir["*"]
+  end
+
+  def post_install
+    resource('latest').clear_cache if build.head?
   end
 end
