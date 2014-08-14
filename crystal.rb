ad8b7a937e10f6c3cc04a86edd9ddab9569d943d
Ary Borenszweig <aborenszweig@manas.com.ar>
2014-08-14 01:58:14 -0300

    Crystal 0.4.3

--- crystal.rb
+++ crystal.rb
@@ -4,8 +4,8 @@ class Crystal < Formula
   homepage 'http://crystal-lang.org/'
 
   stable do
-    url 'http://crystal-lang.s3.amazonaws.com/crystal-darwin-0.4.2.tar.gz'
-    sha1 'a8aaf53bca836aec939e52d1f8a2eab0de7538bf'
+    url 'http://crystal-lang.s3.amazonaws.com/crystal-darwin-0.4.3.tar.gz'
+    sha1 'c9a8aee51f87ff0b3aa2383bb09085883993fbee'
   end
 
   head do
