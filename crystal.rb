b48b993c2fccaac92d23760de974f08d7855a3b1
Juan Wajnerman <jwajnerman@manas.com.ar>
2014-09-29 18:47:18 -0300

    Crystal 0.5.0

--- crystal.rb
+++ crystal.rb
@@ -2,37 +2,45 @@ require 'formula'
 
 class Crystal < Formula
   homepage 'http://crystal-lang.org/'
+  version '0.5.0'
 
   stable do
-    url 'http://crystal-lang.s3.amazonaws.com/crystal-darwin-0.4.4.tar.gz'
-    sha1 '4842c4a42ecdc02fc9eb5437bd564cbb21d6fc1c'
+    url 'http://crystal-lang.s3.amazonaws.com/crystal-0.5.0-1-darwin-x86_64.tar.gz'
+    sha1 '3f5446c53114d9bf0be7d456c689d363067e3186'
   end
 
-  head do
-    url 'http://github.com/manastech/crystal.git'
+  # head do
+  #   url 'http://github.com/manastech/crystal.git'
 
-    resource 'latest' do
-      url 'http://crystal-lang.s3.amazonaws.com/crystal-darwin-latest.tar.gz'
-    end
-  end
+  #   resource 'latest' do
+  #     url 'http://crystal-lang.s3.amazonaws.com/crystal-darwin-latest.tar.gz'
+  #   end
+  # end
 
-  depends_on "llvm33" => [:recommended, 'with-clang', 'all-targets']
-  depends_on "bdw-gc"
+  depends_on "llvm" => :optional
   depends_on "libpcl" => :recommended
 
   def install
-    if build.head?
-      resource('latest').stage do
-        (prefix/"deps").install "bin/crystal-exe" => "crystal"
-      end
+    # if build.head?
+    #   resource('latest').stage do
+    #     (prefix/"deps").install "bin/crystal-exe" => "crystal"
+    #   end
 
-      script_root = %Q(SCRIPT_ROOT="#{prefix}")
-    else
-      script_root = %Q(SCRIPT_ROOT="#{prefix}/bin")
+    #   script_root = %Q(INSTALL_DIR="#{prefix}")
+    # end
+
+    script_root = %Q(INSTALL_DIR="#{prefix}")
+    inreplace('bin/crystal') do |s|
+      s.gsub! /INSTALL_DIR=.+/, script_root
     end
 
-    inreplace('bin/crystal') do |s|
-      s.gsub! /SCRIPT_ROOT=.+/, script_root
+    if build.with?('llvm')
+      inreplace('bin/crystal') do |s|
+        if s =~ /export PATH="(.*)"/
+          llvm_path = Formula["llvm"].opt_prefix
+          s.gsub! /export PATH="(.*)"/, %(export PATH="#{llvm_path}/bin:#{$1}")
+        end
+      end
     end
 
     prefix.install Dir["*"]
