114a736714b8d788c6412550d9b21642bbb3588e
Ary Borenszweig <aborenszweig@manas.com.ar>
2015-04-30 17:45:20 -0300

    Crystal 0.7.0

--- crystal.rb
+++ crystal.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
-CRYSTAL_VERSION = "0.6.1"
-CRYSTAL_SHA = "681fc7806c1d923cd585e9c0a1d193a305e51ddd"
+CRYSTAL_VERSION = "0.7.0"
+CRYSTAL_SHA = "45cfe890380d5acc046f9ecda5413273218e496e"
 
 class Crystal < Formula
   homepage 'http://crystal-lang.org/'
