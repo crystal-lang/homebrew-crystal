17757cd15f5090838b1dc1274f112842457decc9
Ary Borenszweig <aborenszweig@manas.com.ar>
2015-10-30 01:54:19 -0300

    Crystal 0.9.1 and shards 0.5.3

--- shards.rb
+++ shards.rb
@@ -3,7 +3,7 @@ require "formula"
 class Shards < Formula
   homepage "https://github.com/ysbaddaden/shards"
   head "https://github.com/ysbaddaden/shards.git"
-  url "https://github.com/ysbaddaden/shards/archive/v0.5.1.tar.gz"
+  url "https://github.com/ysbaddaden/shards/archive/v0.5.3.tar.gz"
   sha256 "95916792766e42e3b005b144190c3f88d5cb8bcbaaddeb8fa8ced8bac1ef424d"
 
   depends_on "crystal-lang" => :build
