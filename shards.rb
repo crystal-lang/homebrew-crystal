c97094bea356963c81a85fb02fda2b15e30f292b
Juan Wajnerman <jwajnerman@manas.com.ar>
2015-11-04 13:40:38 +0700

    Fix shards sha256 for 0.5.3

--- shards.rb
+++ shards.rb
@@ -4,7 +4,7 @@ class Shards < Formula
   homepage "https://github.com/ysbaddaden/shards"
   head "https://github.com/ysbaddaden/shards.git"
   url "https://github.com/ysbaddaden/shards/archive/v0.5.3.tar.gz"
-  sha256 "95916792766e42e3b005b144190c3f88d5cb8bcbaaddeb8fa8ced8bac1ef424d"
+  sha256 "33a42709dc7f69b892f551b6a2d44b49d9d75b6e54e186fcb7534c8485f90139"
 
   depends_on "crystal-lang" => :build
 
