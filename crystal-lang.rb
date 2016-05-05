1161778c0b07dfd580d50a4fe3388cb11a6e5f76
Ary Borenszweig <aborenszweig@manas.com.ar>
2016-05-05 14:42:53 -0300

    Crystal 0.16.0

--- crystal-lang.rb
+++ crystal-lang.rb
@@ -1,19 +1,19 @@
 class CrystalLang < Formula
   desc "Fast and statically typed, compiled language with Ruby-like syntax"
   homepage "http://crystal-lang.org/"
-  url "https://github.com/crystal-lang/crystal/archive/0.15.0.tar.gz"
-  sha256 "d79445ec92faa2a045af150fca4886d90ecd9fba27451003b68118c8714b26bd"
+  url "https://github.com/crystal-lang/crystal/archive/0.16.0.tar.gz"
+  sha256 "6bc77686da5cb7d1bf34397d6e57381d301a74d97420d1fb5ea7f13c4e8e1dc8"
   head "https://github.com/crystal-lang/crystal.git"
 
   resource "boot" do
-    url "https://github.com/crystal-lang/crystal/releases/download/0.14.2/crystal-0.14.2-1-darwin-x86_64.tar.gz"
-    version "0.14.2"
-    sha256 "f75036b1950035b49a73faa125acd9ff031cecab7020f15cd4db4c4ee6417bfa"
+    url "https://github.com/crystal-lang/crystal/releases/download/0.15.0/crystal-0.15.0-1-darwin-x86_64.tar.gz"
+    version "0.15.0"
+    sha256 "97b35279d4e6e0248e7824753711f3e40c442cba0c94a5b9ea52dc7ab156b668"
   end
 
   resource "shards" do
-    url "https://github.com/ysbaddaden/shards/archive/v0.6.2.tar.gz"
-    sha256 "11d22086d736598efa87eea558e7b304d538372f017fce9bb21476e40c586110"
+    url "https://github.com/ysbaddaden/shards/archive/v0.6.3.tar.gz"
+    sha256 "5245aebb21af0a5682123732e4f4d476e7aa6910252fb3ffe4be60ee8df03ac2"
   end
 
   option "without-release", "Do not build the compiler in release mode"
