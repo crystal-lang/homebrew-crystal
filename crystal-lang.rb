a06339670289aebc1c496201df451a70048c4018
Ary Borenszweig <aborenszweig@manas.com.ar>
2017-01-20 16:30:12 -0300

    Crystal 0.20.5

--- crystal-lang.rb
+++ crystal-lang.rb
@@ -1,14 +1,14 @@
 class CrystalLang < Formula
   desc "Fast and statically typed, compiled language with Ruby-like syntax"
   homepage "http://crystal-lang.org/"
-  url "https://github.com/crystal-lang/crystal/archive/0.20.4.tar.gz"
-  sha256 "fd099f278b71bbb5cad1927c93933d1feba554fbf8f6f4ab9165f535765f5e31"
+  url "https://github.com/crystal-lang/crystal/archive/0.20.5.tar.gz"
+  sha256 "ee1e5948c6e662ccb1e62671cf2c91458775b559b23d74ab226dc2a2d23f7707"
   head "https://github.com/crystal-lang/crystal.git"
 
   resource "boot" do
-    url "https://github.com/crystal-lang/crystal/releases/download/0.20.3/crystal-0.20.3-1-darwin-x86_64.tar.gz"
-    version "0.20.3"
-    sha256 "0088972c5cad9543f262976ae6c8ee1dbcbefdee3a8bedae851998bfa7098637"
+    url "https://github.com/crystal-lang/crystal/releases/download/0.20.4/crystal-0.20.4-1-darwin-x86_64.tar.gz"
+    version "0.20.4"
+    sha256 "3fd291a4a5c9eccdea933a9df25446c90d80660a17e89f83503fcb5b6deba03e"
   end
 
   resource "shards" do
